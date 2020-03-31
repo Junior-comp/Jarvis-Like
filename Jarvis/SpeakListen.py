fad806043ae58e819c56061a1074095059ae0a32
Junior Lole <lolejunioreta@gmail.com>
2020-03-31 02:14:42 +0200

    start jarvis project like

---
+++ Jarvis/SpeakListen.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# Requires PyAudio and PySpeech.
+import speech_recognition as sr
+from gtts import *
+
+class SpeakList():
+	def __init(self):
+		self.__word = None
+
+	def Listen(self):
+		r = sr.Recognizer()
+		with sr.Microphone() as source:
+			audio = r.listen(source)
+		try:
+			s = (r.recognize_google(audio))
+			self.__word = s
+			print(s.lower())
+		except sr.UnknownValueError:
+        print("Don't understand what you are saying mister")
+        except sr.RequestError as e:
+        print("Could not request results$; {0}".format(e))
+
+    def speak(self,output):
+
+
