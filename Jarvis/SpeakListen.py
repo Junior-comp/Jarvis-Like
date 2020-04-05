0979535e412bcb385dd9919c1a35b6d717fb5242
Junior Lole <lolejunioreta@gmail.com>
2020-04-05 17:29:42 +0200

    changing propity

--- Jarvis/SpeakListen.py
+++ Jarvis/SpeakListen.py
@@ -4,22 +4,26 @@ import speech_recognition as sr
 from gtts import *
 
 class SpeakList():
-	def __init(self):
-		self.__word = None
+    def __init__(self):
+        self.__word = None
 
-	def Listen(self):
-		r = sr.Recognizer()
-		with sr.Microphone() as source:
-			audio = r.listen(source)
-		try:
-			s = (r.recognize_google(audio))
-			self.__word = s
-			print(s.lower())
-		except sr.UnknownValueError:
-        print("Don't understand what you are saying mister")
+    def Listen(self):
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            audio = r.listen(source)
+        try:
+            s = (r.recognize_google(audio))
+            self.__word = s
+            print(s.lower())
+        except sr.UnknownValueError:
+            print("Don't understand what you are saying mister")
         except sr.RequestError as e:
-        print("Could not request results$; {0}".format(e))
+            print("Could not request results$; {0}".format(e))
 
-    def speak(self,output):
+    def print(self):
+        if self.__word != None:
+            print(self.__word)
 
-
+a = SpeakList()
+a.Listen()
+a.print()
