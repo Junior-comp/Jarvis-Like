861a85586c35f94e4dc598e041664d2337036638
Junior Lole <lolejunioreta@gmail.com>
2020-04-21 01:57:27 +0200

    fixing listeningvoice function

--- Jarvis/SpeakListen.py
+++ Jarvis/SpeakListen.py
@@ -11,6 +11,7 @@ import wikipedia
 import webbrowser
 import smtplib
 from playsound import playsound
+import sounddevice as sd
 
 
 class SpeakList():
@@ -25,7 +26,7 @@ class SpeakList():
             print("i'm listenning")
             audio = r.listen(source)
         try:
-            s = r.recognize_google(audio,Language = 'en-in')
+            s = r.recognize_google(audio,language = 'fr-FR')
             self.__word = s
             print(s.lower())
         except sr.UnknownValueError:
@@ -63,20 +64,46 @@ class Task:
         al = datetime.strftime(n,"%H:%M:%S").split(":")
         str = "it is {} hour {} minutes {} seconds".format(al[0],al[1],al[2])
         return str
+
     def playmusic(self,str):
         path = "C:\\Users\\lolej\\Music"
         musics = os.listdir(path)
         bool = False
         i = 0
         while i < len(musics):
-            if str in musics[i]:
+            if inside(str,musics[i]):
                 bool = True
                 break
             else:
                 i +=1
         if i < len(musics) and bool == True:
-            playsound(musics[i])
+            playsound(os.path.join(path,musics[i]))
+            #sos.startfile(os.path.join(path,musics[i]))
         return bool
+    
+    def inside(self,str,strcomp):
+        bool = False
+        #on va regarder si le mot se trouve à l'interieur
+        i = 0
+        j = 0
+        while i < len(str):
+            if j<= len(strcomp)-1:
+                if str[i].lower() == strcomp[j].lower():
+                    if bool == True:
+                        j = j+1
+                    else:
+                        bool = True
+                        j = j+1
+                else:
+                    j = 0
+                    bool = false
+            else:
+                break
+            i = i + 1
+        return bool
+
+    def openbrowser():
+        print("trouver comment faire")
 
 
 
@@ -87,6 +114,19 @@ class Task:
     
 
 
-a = SpeakList()
+#a = SpeakList()
 t = Task()
-t.playmusic("Sing Me To Sleep")
+#a.Listen()
+print(t.inside("Junioreta", "JunIOR"))
+
+"""
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+write('', fs, myrecording)  # Save as WAV file
+"""
